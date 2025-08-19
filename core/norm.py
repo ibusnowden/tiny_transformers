89f269c39b51bbd81a8509e852f3e33c0025de0e
ibusnowden <iniang1@southwest.tn.edu>
2025-08-19 11:16:51 -0500

    added the norm for hyperparameters

--- core/norm.py
+++ core/norm.py
@@ -1,1 +1,13 @@
 # Layer Normalization
+import numpy as np
+
+class LayerNorm:
+    def __init__(self, d_model, eps=1e-6):
+        self.gamma = np.ones(d_model)
+        self.beta = np.zeros(d_model)
+        self.eps = eps
+
+    def forward(self, X):
+        mean = np.mean(X, axis=-1, keepdims=True)
+        var = np.var(X, axis=-1, keepdims=True)
+        return self.gamma * (X - mean) / np.sqrt(var + self.eps) + self.beta
